d35057ba3b764244e64038bf2fc21c8126f088aa
ahmed <ahmednoorzakaria680@gmail.com>
2023-12-26 20:49:22 +0300

    hidding api keys

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,4 +1,5 @@
 import user from "../models/user.js";
+import { hash } from 'bcrypt';
 export const getAllUsers = async (req, res, next) => {
     // get all users
     try {
@@ -7,6 +8,19 @@ export const getAllUsers = async (req, res, next) => {
     }
     catch (error) {
         return res.status(500).json({ message: "Error", cause: error });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const User = new user({ name, email, password: hashedPassword });
+        await User.save();
+        return res.status(200).json({ message: "ok", id: User._id.toString() });
+    }
+    catch (error) {
+        console.error(error); // Log the error for debugging purposes
+        return res.status(500).json({ message: "Error", cause: error.message });
     }
 };
 //# sourceMappingURL=user-controllers.js.map
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import user from "../models/user.js";
+import {hash} from 'bcrypt'
 
 export const getAllUsers = async (
   req: Request,
@@ -14,3 +15,20 @@ export const getAllUsers = async (
     return res.status(500).json({ message: "Error", cause: error });
   }
 };
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await hash(password, 10);
+    const User = new user({ name, email, password: hashedPassword });
+    await User.save();
+    return res.status(200).json({ message: "ok", id: User._id.toString() });
+  } catch (error) {
+    console.error(error); // Log the error for debugging purposes
+    return res.status(500).json({ message: "Error",  cause: error.message });
+  }
+};
+
