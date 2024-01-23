047cf6ed9f8193a5ad39170437eea906605873a2
Ahmednoor <Ahmednoorzakaria680@gmail.com>
2024-01-23 07:48:54 +0000

    Added cookie and jwt tokens

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
-import {config} from "dotenv";
+import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 config();
-const app = express()
+const app = express();
+
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
-
-app.use("/api/v1",appRouter);
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use("/api/v1", appRouter);
 
 export default app;
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,16 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import user from "../models/user.js";
-import {hash , compare} from 'bcrypt'
+import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manageger.js";
 
 export const getAllUsers = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   // get all users
   try {
     const users = await user.find();
-    return res.status(200).json({ message: "ok" ,users});
+    return res.status(200).json({ message: "ok", users });
   } catch (error) {
     return res.status(500).json({ message: "Error", cause: error });
   }
@@ -18,45 +19,77 @@ export const getAllUsers = async (
 export const userSignup = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     //user signup
     console.log("Request body:", req.body);
     const { name, email, password } = req.body;
-    const existingUser = await user.findOne({email});
-    if(existingUser)return res.status(401).send("user already registered")
+    const existingUser = await user.findOne({ email });
+    if (existingUser) return res.status(401).send("user already registered");
     const hashedPassword = await hash(password, 10);
     const User = new user({ name, email, password: hashedPassword });
     await User.save();
+    res.clearCookie(COOKIE_NAME,{
+      path:"/";
+      domain:"localhost",
+      httpOnly:true,
+      signed:true,     
+    });
+    const token = createToken(User._id.tostring(), User.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: "ok", id: User._id.toString() });
   } catch (error) {
     console.error(error); // Log the error for debugging purposes
-    return res.status(500).json({ message: "Error",  cause: error.message });
+    return res.status(500).json({ message: "Error", cause: error.message });
   }
 };
 
 export const userLogin = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     //user login
     const { email, password } = req.body;
-    const User = await user.findOne({email});
-    if(!User){
-      return res.status(401).send('User not registered')
+    const User = await user.findOne({ email });
+    if (!User) {
+      return res.status(401).send("User not registered");
     }
-    const isPassword = await compare(password,User.password);
-    if(!isPassword){
-      return res.status(403).send("Incorrect password")
+    const isPassword = await compare(password, User.password);
+    if (!isPassword) {
+      return res.status(403).send("Incorrect password");
     }
+    res.clearCookie(COOKIE_NAME,{
+      path:"/";
+      domain:"localhost",
+      httpOnly:true,
+      signed:true,     
+    });
+    const token = createToken(User._id.tostring(), User.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
 
-  return res.status(200).json({message:"ok",id:User._id.toString()})
-
+    return res.status(200).json({ message: "ok", id: User._id.toString() });
   } catch (error) {
     console.error(error); // Log the error for debugging purposes
-    return res.status(500).json({ message: "Error",  cause: error.message });
+    return res.status(500).json({ message: "Error", cause: error.message });
   }
 };
