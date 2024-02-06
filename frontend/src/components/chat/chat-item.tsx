c5e18a11887a29d439b5be459b4b58bf49895b47
Ahmednoor <Ahmednoorzakaria680@gmail.com>
2024-02-06 13:42:00 +0000

    connceted open api with chatbot

--- frontend/src/components/chat/chat-item.tsx
+++ frontend/src/components/chat/chat-item.tsx
@@ -1,6 +1,31 @@
 import React from "react";
 import { Avatar, Box, Typography } from "@mui/material";
 import { useAuth } from "../../context/AuthContext";
+import { prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkdark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+function extractCodeFromString(message) {
+  if (message.includes("```")) {
+    const blocks = message.split("```");
+    return blocks;
+  }
+}
+
+function isCodeBlock(str) {
+  if (
+    str.includes("=") ||
+    str.includes(";") ||
+    str.includes("[") ||
+    str.includes("]") ||
+    str.includes("{") ||
+    str.includes("}") ||
+    str.includes("#") ||
+    str.includes("//")
+  ) {
+    return true;
+  }
+  return false;
+}
 
 export const ChatItem = ({
   content,
@@ -9,7 +34,9 @@ export const ChatItem = ({
   content: string;
   role: "user" | "assistant";
 }) => {
+  const messageBlocks = extractCodeFromString(content);
   const auth = useAuth();
+
   return role === "assistant" ? (
     <Box
       sx={{
@@ -21,10 +48,28 @@ export const ChatItem = ({
       }}
     >
       <Avatar sx={{ ml: "0" }}>
-        <img src="openai.png" width={"30px"} />
+        <img src="openai.png" width={"30px"} alt="OpenAI Avatar" />
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks ? (
+          <Typography fontSize={"20px"}>{content}</Typography>
+        ) : (
+          messageBlocks.map((block, index) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter
+                key={index}
+                style={coldarkdark}
+                language="javascript"
+              >
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography key={index} fontSize={"20px"}>
+                {block}
+              </Typography>
+            ),
+          )
+        )}
       </Box>
     </Box>
   ) : (
@@ -36,12 +81,31 @@ export const ChatItem = ({
         bgcolor: "#004d56",
       }}
     >
-      <Avatar sx={{ ml: "0",bgcolor:"black",color:"white" }}>
-        {auth?.user?.name[0]},{auth?.user?.name.split("")[1][0]}
+      <Avatar sx={{ ml: "0", bgcolor: "black", color: "white" }}>
+        {auth?.user?.name[0]}, {auth?.user?.name.split("")[1][0]}
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks ? (
+          <Typography fontSize={"20px"}>{content}</Typography>
+        ) : (
+          messageBlocks.map((block, index) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter
+                key={index}
+                style={coldarkdark}
+                language="javascript"
+              >
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography key={index} fontSize={"20px"}>
+                {block}
+              </Typography>
+            ),
+          )
+        )}
       </Box>
+      B{" "}
     </Box>
   );
 };
--- frontend/src/pages/chat.tsx
+++ frontend/src/pages/chat.tsx
@@ -3,53 +3,27 @@ import React, { useRef } from "react";
 import { useAuth } from "../context/AuthContext";
 import { red } from "@mui/material/colors";
 import { ChatItem } from "../components/chat/chat-item";
-import { IoMdSend } from 'react-icons/io';
-
+import { IoMdSend } from "react-icons/io";
+import { sendChatRequest } from "../helpers/api-communicator";
+type Message = {
+  role: "user" | "assistant";
+  content: string;
+};
 const Chat = () => {
   const auth = useAuth();
-  const inputRef=useRef<HTMLInputElement | null>(null)
-  const handleSubmit = async()=>{
+  const [chatMessages, setChatMessages] = React.useState<Message[]>([]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const handleSubmit = async () => {
     console.log(inputRef.current?.value);
-  }
-  const chatMessages = [
-    { role: "user", content: "Hi there!" },
-    { role: "assistant", content: "Hello! How can I assist you today?" },
-    { role: "user", content: "I need help with a programming problem." },
-    {
-      role: "assistant",
-      content: "Sure, I'd be happy to help. What's the issue you're facing?",
-    },
-    {
-      role: "user",
-      content: "I'm getting an error when trying to compile my code.",
-    },
-    {
-      role: "assistant",
-      content:
-        "Could you please provide the error message and a snippet of your code?",
-    },
-    {
-      role: "user",
-      content:
-        "Here's the error: 'Undefined reference to main'. And here's a snippet of my code:",
-    },
-    {
-      role: "user",
-      content: "int main() {\n  // Your code here\n  return 0;\n}",
-    },
-    {
-      role: "assistant",
-      content:
-        "It looks like your program might be missing a definition for the main function. Make sure you have a complete implementation within the 'main' function.",
-    },
-    { role: "user", content: "Oh, I see. Let me fix that." },
-    {
-      role: "assistant",
-      content:
-        "Take your time. If you have any more questions, feel free to ask.",
-    },
-    // Additional messages can be added as needed
-  ];
+    const content = inputRef.current?.value as string;
+    if (inputRef && inputRef.current) {
+      inputRef.current.value = "";
+    }
+    const newMessage: Message = { role: "user", content };
+    setChatMessages((prev) => [...prev, newMessage]);
+    const chatData = await sendChatRequest(content);
+    setChatMessages([...chatData.chats]);
+  };
   return (
     <Box
       sx={{
@@ -125,7 +99,13 @@ const Chat = () => {
         }}
       >
         <Typography
-          sx={{ mx: "auto", fontSize: "40px", color: "white", mb: 2 ,fontWeight: "600",}}
+          sx={{
+            mx: "auto",
+            fontSize: "40px",
+            color: "white",
+            mb: 2,
+            fontWeight: "600",
+          }}
         >
           Model - GPT 3.5 Turbo
         </Typography>
@@ -151,27 +131,32 @@ const Chat = () => {
       </Box>
       <div
         style={{
-           width: "100%",
-            borderRadius: 8,
-            backgroundColor: "rgb(17,27,39)",
-            display: "flex",
-            margin: "auto",
+          width: "100%",
+          borderRadius: 8,
+          backgroundColor: "rgb(17,27,39)",
+          display: "flex",
+          margin: "auto",
         }}
       >
         <input
-        ref={inputRef}
+          ref={inputRef}
           type="text"
           style={{
-             width: "100%",
-              backgroundColor: "transparent",
-              padding: "30px",
-              border: "none",
-              outline: "none",
-              color: "white",
-              fontSize: "20px",
+            width: "100%",
+            backgroundColor: "transparent",
+            padding: "30px",
+            border: "none",
+            outline: "none",
+            color: "white",
+            fontSize: "20px",
           }}
         ></input>
-        <IconButton sx={{marginLeft:"auto",color:"white"}} onClick={handleSubmit}><IoMdSend/></IconButton>
+        <IconButton
+          sx={{ marginLeft: "auto", color: "white" }}
+          onClick={handleSubmit}
+        >
+          <IoMdSend />
+        </IconButton>
       </div>
     </Box>
   );
